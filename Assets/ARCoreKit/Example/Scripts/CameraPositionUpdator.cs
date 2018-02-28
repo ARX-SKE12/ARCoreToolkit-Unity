9e6e70ffcbab89e8e177833d3ce3c1f66445f733
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-28 15:31:33 +0700

    Init Kit Log Cam Pos

---
+++ Assets/ARCoreKit/Example/Scripts/CameraPositionUpdator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraPositionUpdator : MonoBehaviour {
+
+    public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.GetComponent<Text>().text = "Cam Pos: "+transform.position.ToString();
+	}
+}
