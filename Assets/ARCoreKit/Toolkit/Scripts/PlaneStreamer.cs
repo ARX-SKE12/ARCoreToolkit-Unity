62fd52eafd8f7892a694a94e9c8424dbb00a851c
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-02 00:16:44 +0700

    Add namespace ARToolkit

--- Assets/ARCoreKit/Toolkit/Scripts/PlaneStreamer.cs
+++ Assets/ARCoreKit/Toolkit/Scripts/PlaneStreamer.cs
@@ -2,64 +2,70 @@
 using GoogleARCore;
 using PublisherKit;
 
-public class PlaneStreamer : Publisher {
-
-#region Attributes
-    List<TrackedPlane> newPlanes, allPlanes;
-    #endregion
-
-#region Events
-    const string NEW_PLANES_EVENT = "OnNewPlanesDetected";
-#endregion
-
-#region Untiy Behaviour
-    void Awake()
+namespace ARToolkit
+{
+    public class PlaneStreamer : Publisher
     {
-        InitializePlaneCollector();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        StreamPlanes();
-	}
-#endregion
 
-#region Plane Collector
-    void InitializePlaneCollector()
-    {
-        newPlanes = new List<TrackedPlane>();
-        allPlanes = new List<TrackedPlane>();
-    }
-#endregion
-        
-#region Planes Streamer
-    void StreamPlanes()
-    {
-        if (IsTracking())
+        #region Attributes
+        List<TrackedPlane> newPlanes, allPlanes;
+        #endregion
+
+        #region Events
+        const string NEW_PLANES_EVENT = "OnNewPlanesDetected";
+        #endregion
+
+        #region Untiy Behaviour
+        void Awake()
         {
-            StreamNewPlanes();
+            InitializePlaneCollector();
         }
-    }
 
-    void StreamNewPlanes()
-    {
-        Session.GetTrackables<TrackedPlane>(newPlanes, TrackableQueryFilter.New);
-        Broadcast(NEW_PLANES_EVENT, newPlanes);
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            StreamPlanes();
+        }
+        #endregion
 
-    public List<TrackedPlane> GetAllTrackedPlane()
-    {
-        if (!IsTracking()) return null;
-        Session.GetTrackables<TrackedPlane>(allPlanes, TrackableQueryFilter.All);
-        return allPlanes;
-    }
-#endregion
+        #region Plane Collector
+        void InitializePlaneCollector()
+        {
+            newPlanes = new List<TrackedPlane>();
+            allPlanes = new List<TrackedPlane>();
+        }
+        #endregion
 
-#region Tracking Status
-    bool IsTracking()
-    {
-        return Session.Status == SessionStatus.Tracking;
+        #region Planes Streamer
+        void StreamPlanes()
+        {
+            if (IsTracking())
+            {
+                StreamNewPlanes();
+            }
+        }
+
+        void StreamNewPlanes()
+        {
+            Session.GetTrackables<TrackedPlane>(newPlanes, TrackableQueryFilter.New);
+            Broadcast(NEW_PLANES_EVENT, newPlanes);
+        }
+
+        public List<TrackedPlane> GetAllTrackedPlane()
+        {
+            if (!IsTracking()) return null;
+            Session.GetTrackables<TrackedPlane>(allPlanes, TrackableQueryFilter.All);
+            return allPlanes;
+        }
+        #endregion
+
+        #region Tracking Status
+        bool IsTracking()
+        {
+            return Session.Status == SessionStatus.Tracking;
+        }
+        #endregion
+
     }
-#endregion
 
 }
