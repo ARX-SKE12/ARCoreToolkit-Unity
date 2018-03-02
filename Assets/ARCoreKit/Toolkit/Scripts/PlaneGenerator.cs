d1f7855aeed6c766c72d7fb2d2db0202a645dc0c
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-02 16:40:43 +0700

    Indent Fix

--- Assets/ARCoreKit/Toolkit/Scripts/PlaneGenerator.cs
+++ Assets/ARCoreKit/Toolkit/Scripts/PlaneGenerator.cs
@@ -6,30 +6,30 @@ namespace ARCoreToolkit
     public class PlaneGenerator : MonoBehaviour
     {
 
-    #region Plane Components
+        #region Plane Components
         public Material planeMaterial;
         public GameObject planePrefab;
     #endregion
 
-    #region Unity Behaviour
-        // Use this for initialization
-        void Start()
-        {
-            GameObject.FindObjectOfType<PlaneStreamer>().Register(gameObject);
-        }
-    #endregion
-
-    #region Plane Detecting
-        void OnNewPlanesDetected(List<TrackedPlane> trackedPlanes)
-        {
-            foreach (TrackedPlane plane in trackedPlanes)
+        #region Unity Behaviour
+            // Use this for initialization
+            void Start()
             {
-                GameObject planeObject = Instantiate(planePrefab, Vector3.zero, Quaternion.identity, transform);
-                planeObject.GetComponent<PlaneBehaviour>().InitializePlane(plane);
-                planeObject.GetComponent<MeshRenderer>().material = planeMaterial;
+                GameObject.FindObjectOfType<PlaneStreamer>().Register(gameObject);
             }
-        }
-    #endregion
+        #endregion
+
+        #region Plane Detecting
+            void OnNewPlanesDetected(List<TrackedPlane> trackedPlanes)
+            {
+                foreach (TrackedPlane plane in trackedPlanes)
+                {
+                    GameObject planeObject = Instantiate(planePrefab, Vector3.zero, Quaternion.identity, transform);
+                    planeObject.GetComponent<PlaneBehaviour>().InitializePlane(plane);
+                    planeObject.GetComponent<MeshRenderer>().material = planeMaterial;
+                }
+            }
+        #endregion
 
     }
 }
